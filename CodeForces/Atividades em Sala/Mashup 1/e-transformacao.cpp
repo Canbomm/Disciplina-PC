cca2cb8487a757e23eb3e29ccf63c4ff7b6a0c32
Canbomm <canbomim@hotmail.com>
2024-04-29 20:09:04 -0300

    Primos solved

--- CodeForces/Atividades em Sala/Mashup 1/e-transformacao.cpp
+++ CodeForces/Atividades em Sala/Mashup 1/e-transformacao.cpp
@@ -1,51 +1,15 @@
-#include <bits/stdc++.h>
-#define llint long long int
- 
-using namespace std;
- 
-bool divide_primo(llint num){
-  llint primo = 3;
-  llint test = 3;
-  while(primo <= num){
-    if(num%primo == 0){
-      return true;
-    }
-    primo = primo + 2;
-    test = 3;
-    while(test < primo){
-      if(primo%test == 0){
-        primo = primo + 2;
-        test = 3;
-      }
-      else{
-        test = test + 2;
-      }
-    }
-  }
-  return false;
-}
+#include <cstdio>
  
 int main(){
-  int cases;
-  llint x,y;
-  scanf("%d",&cases);
-  getchar();
-  for(int i = 0; i < cases; i++){
+  int t;
+  long long int x,y;
+  scanf("%d",&t);
+  for(int i = 0; i < t;i++){
     scanf("%lld %lld",&x,&y);
-    llint diff = x - y;
-    if(diff == 1){
+    if((x-y) == 1)
       printf("NO\n");
-    }
-    else if(diff%2 == 0){
+    else
       printf("YES\n");
-    }
-    else if(divide_primo(diff)){
-      printf("YES\n");
-    }
-    else{
-      printf("NO\n");
-    }
-    
   }
   return 0;
 }
