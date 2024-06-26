fc7a8b6fbebcfd3515dd141825cd715ea96bf90b
Canbomm <canbomim@hotmail.com>
2024-06-25 21:24:55 -0300

    Lista VI (tentativa)

--- CodeForces/Listas/Lista de exercícios 6/b-tabuleiroDFS.cpp
+++ CodeForces/Listas/Lista de exercícios 6/b-tabuleiroDFS.cpp
@@ -1,8 +1,11 @@
+// Nao funciona
 #include <bits/stdc++.h>
 
 using namespace std;
 
 char tabuleiro[102][102];
+map<char,char> inverso;
+int problemas = 0;
 
 void printTabuleiro(int lin, int col){
   for(int i = 1; i <= lin; i++){
@@ -35,22 +38,32 @@ bool verifica(int lin, int col, char peca){
   return true;
 }
 
-void dfs(int lin, int col,char inicial){
+void dfs(int lin, int col,int posX, int posY, char tentativa){
   char peca;
-  for(int i = 1; i <= lin; i++){
-    for(int j = 1; j <= col; j++){
+  for(int i = posX; i <= lin; i++){
+    for(int j = posY; j <= col; j++){
       peca = tabuleiro[i][j];
       if(peca == '.'){
-        tabuleiro[i][j] = inicial;
-        if(!verifica(i,j,inicial))
-          tabuleiro[i][j] = inverso[inicial];
+        tabuleiro[i][j] = tentativa;
+        if(!verifica(i,j,tentativa)){
+          problemas++;
+          tabuleiro[i][j] = '.';
+          if(problemas < 1)
+            dfs(lin,col,i,j,inverso[tentativa]);
+          else{
+            problemas = 0;
+            return;
+          }
+        }
       }
     }
   }
-  return true;
 }
 
 int main(){
+  inverso['B'] = 'W';
+  inverso['W'] = 'B';
+
   int lin,col; // linha e coluna
   char peca;
   scanf("%d %d",&lin,&col);
@@ -64,7 +77,8 @@ int main(){
     }
     getchar();
   }
-  dfs(lin,col,'W');
+  
+  dfs(lin,col,1,1,'B');
   
   printTabuleiro(lin,col);
   return 0; // \n
--- CodeForces/Listas/Lista de exercícios 6/c-paredes em labirintos.cpp
+++ CodeForces/Listas/Lista de exercícios 6/c-paredes em labirintos.cpp
@@ -1,12 +1,114 @@
+// Extremamente ineficiente
 #include <bits/stdc++.h>
 
-// ios::sync_with_stdio(false);
-// cin.tie(0);
-// cout.tie(0);
-// typedef ORIGINAL NEW;
-
 using namespace std;
+typedef pair<int, int> pii;
+
+const int max_size = 502;
+char mapa[max_size][max_size];
+bool visitados[max_size][max_size];
+
+void resetaMapa(int n, int m){
+  for(int i = 0; i < n+2; i++){
+    for(int j = 0; j < m+2; j++)
+      mapa[i][j] = '#';
+  }
+}
+
+void resetaVisitados(int n, int m){
+  for(int i = 0; i < n+2; i++){
+    for(int j = 0; j < m+2; j++){
+      visitados[i][j] = false;
+    }
+  }
+}
+
+void printMapa(int n, int m){
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= m; j++){
+      printf("%c",mapa[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+bool contaGrupos(int n, int m){
+  int grupos = 0;
+  queue<pii> atual;
+  int x,y;
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= m; j++){
+      if(mapa[i][j] == '.' && !visitados[i][j]){
+        grupos++;
+        if(grupos > 1)
+          return false;
+        visitados[i][j] = true;
+        atual.push({i,j});
+        while(!atual.empty()){
+          tie(x,y) = atual.front();
+          atual.pop();
+          if(mapa[x+1][y] == '.' && !visitados[x+1][y]){
+            atual.push({x+1,y});
+            visitados[x+1][y] = true;
+          }
+          if(mapa[x-1][y] == '.' && !visitados[x-1][y]){
+            atual.push({x-1,y});
+            visitados[x-1][y] = true;
+          }
+          if(mapa[x][y+1] == '.' && !visitados[x][y+1]){
+            atual.push({x,y+1});
+            visitados[x][y+1] = true;
+          }
+          if(mapa[x][y-1] == '.' && !visitados[x][y-1]){
+            atual.push({x,y-1});
+            visitados[x][y-1] = true;
+          }
+        }
+      }
+    }
+  }
+  return true;
+}
+
+void pareda(int n, int m, int k){
+  while(k > 0){
+    for(int i = 1; i <= n; i++){
+      for(int j = 1; j <= m; j++){
+        if(mapa[i][j] == '.'){
+          mapa[i][j] = 'X';
+          if(!contaGrupos(n,m)){
+            mapa[i][j] = '.';
+          }
+          else{
+            k--;
+            if(k < 1)
+              return;
+          }
+          resetaVisitados(n,m);
+        }
+      }
+    }
+  }
+}
 
 int main(){
+  int n,m,k;
+  char aux;
+  scanf("%d %d %d",&n,&m,&k);
+  getchar();
+  
+  resetaMapa(n,m);
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= m; j++){
+      scanf("%c",&aux);
+      mapa[i][j] = aux;
+    }
+    getchar();
+  }
+
+  pareda(n,m,k);
+
+  printMapa(n,m);
+
   return 0; // \n
 }
